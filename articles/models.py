3344384cee1aa027df4b3b0022ca0e4b8636da34
geksus <alexgergel@gmail.com>
2026-04-13 11:46:21 +0300

    comments

--- articles/models.py
+++ articles/models.py
@@ -14,3 +14,16 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+
+    def get_absolute_url(self):
+        return reverse("article_list")
